eb9c9920430296c0785882764d8ac530e061dc87
Jari Tenhunen <jari.tenhunen@outlook.com>
2024-09-22 10:09:07 +0200

    Exercise1 done

--- src/Main.java
+++ src/Main.java
@@ -22,6 +22,33 @@ public class Main {
         }
     }
     public static void Exercise1(){
-        System.out.println("Helo World");
+        var arr1 = new double[4];
+        var arr2 = new int[]{
+                5,
+                10,
+                20,
+                50,
+                100,
+                200,
+                500
+        };
+        System.out.println("Printing out each element in both arrays with a for loop");
+        System.out.println("Array 1:");
+        for(int i = 0; i < arr1.length; i++){
+            System.out.println(arr1[i]);
+        }
+        System.out.println("Array 2:");
+        for(int j = 0; j < arr2.length; j++){
+            System.out.println(arr2[j]);
+        }
+        System.out.println("Using foreach loop");
+        System.out.println("Array 1:");
+        for(double element1 : arr1){
+            System.out.println(element1);
+        }
+        System.out.println("Array 2:");
+        for(int element2 : arr2){
+            System.out.println(element2);
+        }
     }
 }
